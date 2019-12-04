6ef65093c3da4918bd4ace1c1f1c50b63aa32b39
stevenchiu30801 <steven30801@gmail.com>
2019-12-04 18:32:08 +0800

    add service and configure upf network

--- build/free5gc-build/setup.sh
+++ build/free5gc-build/setup.sh
@@ -9,3 +9,7 @@ ip addr add 45.45.0.1/16 dev uptun
 ip addr del cafe::1/64 dev uptun 2> /dev/null
 ip addr add cafe::1/64 dev uptun
 ip link set uptun up
+# network environment setting
+sh -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -I INPUT -i uptun -j ACCEPT
