d9e8329cd5e154f7feb19198fcbc3c668d995ce1
stevenchiu30801 <steven30801@gmail.com>
2019-12-24 13:44:08 +0800

    enable ipv6 on upf uptun

--- build/free5gc-build/setup.sh
+++ build/free5gc-build/setup.sh
@@ -4,6 +4,7 @@
 if ! grep "uptun" /proc/net/dev > /dev/null; then
     ip tuntap add name uptun mode tun
 fi
+echo 0 > /proc/sys/net/ipv6/conf/uptun/disable_ipv6
 ip addr del 45.45.0.1/16 dev uptun 2> /dev/null
 ip addr add 45.45.0.1/16 dev uptun
 ip addr del cafe::1/64 dev uptun 2> /dev/null
