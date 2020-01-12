7006555b1c8cc12580823816939f6ccde88e912e
stevenchiu30801 <steven30801@gmail.com>
2020-01-12 12:01:20 +0800

    remove unused deploy and reset dockerfile

--- build/free5gc-build/setup.sh
+++ build/free5gc-build/setup.sh
@@ -14,4 +14,3 @@ ip link set uptun up
 sh -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -I INPUT -i uptun -j ACCEPT
-route add -net 192.168.3.0 netmask 255.255.255.0 gw 192.168.4.1 dev net1
